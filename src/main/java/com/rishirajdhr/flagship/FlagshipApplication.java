56458e62d1f68640413d7debb869b5022bb0ec23
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-08-30 17:18:57 -0500

    feat: setup spring boot project

---
+++ src/main/java/com/rishirajdhr/flagship/FlagshipApplication.java
@@ -0,0 +1,12 @@
+package com.rishirajdhr.flagship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagshipApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlagshipApplication.class, args);
+  }
+}
---
+++ src/test/java/com/rishirajdhr/flagship/FlagshipApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rishirajdhr.flagship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagshipApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
