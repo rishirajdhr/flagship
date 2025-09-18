9038d47df48f9d5a06eb40e78042130003cd561d
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-09-18 14:54:41 -0400

    feat(ui): add create flag dialog

--- src/main/ui/app/api/flags.ts
+++ src/main/ui/app/api/flags.ts
@@ -19,6 +19,24 @@ export interface UpdateFlagParams
   extends FlagParams,
     Partial<Pick<Flag, "description" | "enabled">> {}
 
+export interface NewFlagParams
+  extends Pick<Flag, "name" | "description" | "enabled"> {
+  projectId: string;
+}
+
+export async function createFlagForProject(params: NewFlagParams, auth: Auth) {
+  const { projectId, ...body } = params;
+  const result = await fetch(withBase(`/api/projects/${projectId}/flags`), {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${auth.token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return result;
+}
+
 export async function updateFlagForProject(
   params: UpdateFlagParams,
   auth: Auth
--- src/main/ui/app/routes/project.tsx
+++ src/main/ui/app/routes/project.tsx
@@ -1,14 +1,17 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import * as Switch from "@radix-ui/react-switch";
 import { Tooltip, TooltipProvider } from "~/components/ui/tooltip";
 import type { Route } from "./+types/project";
-import { redirect, useFetcher } from "react-router";
+import { Form, redirect, useFetcher, useSearchParams } from "react-router";
 import type React from "react";
 import { cloneElement } from "react";
 import {
+  createFlagForProject,
   deleteFlagForProject,
   getAllFlagsForProject,
   updateFlagForProject,
   type FlagParams,
+  type NewFlagParams,
   type UpdateFlagParams,
 } from "~/api/flags";
 import { authContext } from "~/middleware-context";
@@ -37,6 +40,42 @@ export async function clientLoader({
   return flags.sort((a, b) =>
     a.name.toLowerCase().localeCompare(b.name.toLowerCase())
   );
+}
+
+async function createFlag({
+  context,
+  params,
+  request,
+}: Route.ClientActionArgs) {
+  const auth = context.get(authContext);
+  if (auth === null) {
+    throw redirect("/login");
+  }
+
+  const formData = await request.formData();
+  const name = formData.get("name")?.toString() ?? "";
+  const description = formData.get("description")?.toString() ?? "";
+  const enabled = false;
+  const { projectId } = params;
+  const newFlagParams: NewFlagParams = {
+    projectId,
+    name,
+    description,
+    enabled,
+  };
+
+  const result = await createFlagForProject(newFlagParams, auth);
+
+  if (result.status === 401) {
+    throw redirect("/login");
+  }
+
+  if (!result.ok) {
+    throw new Response("Failed to create flag", { status: result.status });
+  }
+
+  const createdFlag = await result.json();
+  return createdFlag as Flag;
 }
 
 async function updateFlag({
@@ -109,6 +148,8 @@ export async function clientAction(args: Route.ClientActionArgs) {
   }
 
   switch (args.request.method) {
+    case "POST":
+      return await createFlag(args);
     case "PUT":
       return await updateFlag(args);
     case "DELETE":
@@ -116,7 +157,24 @@ export async function clientAction(args: Route.ClientActionArgs) {
   }
 }
 
-export default function Project({ loaderData: flags }: Route.ComponentProps) {
+export default function Project({
+  loaderData: flags,
+  params,
+}: Route.ComponentProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const modal = searchParams.get("modal");
+
+  const handleCreateFlagDialogOpenChange = (open: boolean) => {
+    setSearchParams((prevSearchParams) => {
+      if (open) {
+        prevSearchParams.set("modal", "create-flag");
+      } else {
+        prevSearchParams.delete("modal");
+      }
+      return prevSearchParams;
+    });
+  };
+
   return (
     <main className="p-8">
       <TooltipProvider>
@@ -147,25 +205,139 @@ export default function Project({ loaderData: flags }: Route.ComponentProps) {
                 </svg>
               </span>
             </span>
-            <button className="flex cursor-pointer flex-row items-center justify-center gap-1 rounded-sm bg-orange-500 px-3 py-1.5 text-sm text-white shadow-xs transition-all hover:bg-orange-400 hover:shadow-sm active:bg-orange-600">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-4 translate-[0.5px]"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
-              </span>
-              <span className="tracking-tight">New Flag</span>
-            </button>
+            <Dialog.Root
+              open={modal === "create-flag"}
+              onOpenChange={handleCreateFlagDialogOpenChange}
+            >
+              <Dialog.Trigger asChild>
+                <button className="flex cursor-pointer flex-row items-center justify-center gap-1 rounded-sm bg-orange-500 px-3 py-1.5 text-sm text-white shadow-xs transition-all hover:bg-orange-400 hover:shadow-sm active:bg-orange-600">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-4 translate-[0.5px]"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                  </span>
+                  <span className="tracking-tight">New Flag</span>
+                </button>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="fixed top-0 right-0 bottom-0 left-0 grid place-items-center bg-gray-800/25 backdrop-blur-xs">
+                  <Dialog.Content className="rounded border-gray-300 bg-white shadow">
+                    <Form
+                      method="POST"
+                      action={`/projects/${params.projectId}`}
+                    >
+                      <div className="flex flex-row items-center justify-between border-b border-gray-300 p-4">
+                        <div>
+                          <Dialog.Title className="text-xl font-semibold tracking-tight text-gray-800">
+                            Create Flag
+                          </Dialog.Title>
+                        </div>
+                        <div>
+                          <Dialog.Close asChild>
+                            <button className="flex size-8 items-center justify-center rounded-full hover:bg-gray-200/40">
+                              <span className="text-gray-800">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="size-6"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18 18 6M6 6l12 12"
+                                  />
+                                </svg>
+                              </span>
+                            </button>
+                          </Dialog.Close>
+                        </div>
+                      </div>
+                      <div className="flex flex-col gap-6 p-4">
+                        <div className="flex flex-col gap-2">
+                          <label
+                            htmlFor="name"
+                            className="tracking-tight text-gray-800"
+                          >
+                            Name
+                          </label>
+                          <input
+                            id="name"
+                            name="name"
+                            type="name"
+                            placeholder="Name of the feature flag"
+                            className="w-80 rounded border border-gray-300 px-3 py-1.5 text-sm tracking-tight text-gray-800"
+                          />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                          <label
+                            htmlFor="description"
+                            className="tracking-tight text-gray-800"
+                          >
+                            Description
+                          </label>
+                          <input
+                            id="description"
+                            name="description"
+                            type="description"
+                            placeholder="A description for the feature flag"
+                            className="w-80 rounded border border-gray-300 px-3 py-1.5 text-sm tracking-tight text-gray-800"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex flex-row items-center justify-end gap-2 border-t border-gray-300 p-4">
+                        <Dialog.Close asChild>
+                          <button className="flex w-24 flex-row items-center justify-center gap-2 rounded px-4 py-2 font-medium tracking-tight text-orange-500 transition-colors not-disabled:hover:bg-orange-100/40 not-disabled:active:bg-orange-100/25 disabled:opacity-50">
+                            <span>Cancel</span>
+                          </button>
+                        </Dialog.Close>
+                        <button
+                          // type="submit"
+                          // disabled={isSubmitting}
+                          className="flex w-24 flex-row items-center justify-center gap-2 rounded bg-orange-500 px-4 py-2 font-medium tracking-tight text-white transition-colors not-disabled:hover:bg-orange-400 not-disabled:active:bg-orange-600 disabled:opacity-50"
+                        >
+                          {/* {isSubmitting ? (
+                <>
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="size-5 animate-spin"
+                    >
+                      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+                    </svg>
+                  </span>
+                  <span>Logging in...</span>
+                </>
+              ) : (
+                <span>Login</span>
+              )} */}
+                          <span>Create</span>
+                        </button>
+                      </div>
+                    </Form>
+                  </Dialog.Content>
+                </Dialog.Overlay>
+              </Dialog.Portal>
+            </Dialog.Root>
           </div>
           <div className="overflow-x-auto rounded border border-gray-300">
             <table className="w-full table-fixed divide-y divide-gray-300">
